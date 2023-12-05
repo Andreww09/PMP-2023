de74102f5764a9f847f0bcafb4083bcb57766ac2
Mocanu Octavian <mocanuoctavian12@gmail.com>
2023-12-05 21:06:03 +0200

    Laboratorul 9

--- Lab09/main.py
+++ Lab09/main.py
@@ -13,7 +13,10 @@ adm = data['Admission'].values
 
 x_n = ['GPA', 'GRE']
 x_1 = np.array(data[x_n].values,dtype=np.float64)
+rng = np.random.default_rng(101)
 
+
+# pct a
 with pm.Model() as model_1:
     alpha = pm.Normal('alpha', mu=0, sigma=10)
     beta = pm.Normal('beta', mu=0, sigma=2, shape=len(x_n))
@@ -21,11 +24,13 @@ with pm.Model() as model_1:
     teta = pm.Deterministic('teta', 1 / (1 + pm.math.exp(-mu)))
     bd = pm.Deterministic('bd', -alpha/beta[1] - beta[0]/beta[1] * x_1[:,0])
     yl = pm.Bernoulli('yl', p=teta, observed=adm)
-    idata_1 = pm.sample(50, cores=1,return_inferencedata=True)
+    idata_1 = pm.sample(100, cores=1,return_inferencedata=True,random_seed=rng)
 
 az.plot_posterior(idata_1, var_names=['alpha','beta'])
 plt.show()
 
+
+# pct b
 idx = np.argsort(x_1[:,0])
 bd = idata_1.posterior['bd'].mean(("chain", "draw"))[idx]
 plt.scatter(x_1[:,0], x_1[:,1], c=[f'C{x}' for x in adm])
@@ -34,3 +39,36 @@ az.plot_hdi(x_1[:,0], idata_1.posterior['bd'], color='k',hdi_prob=0.94)
 plt.xlabel(x_n[0])
 plt.ylabel(x_n[1])
 plt.show()
+
+# pct c si d
+def compute_probability(gre,gpa):
+  prob = []
+  alpha0=idata_1.posterior['alpha'][1]
+  betas = idata_1.posterior['beta'][1]
+
+  for i in range(len(alpha0)):
+      prob.append(1 / (1 + np.exp(-(alpha0[i] + gpa * betas[i][0] + gre * betas[i][1]))))
+  prob = np.array(prob)
+  intervals = pm.stats.hdi(prob, hdi_prob=0.95)
+
+  hdi_prob = []
+  for i in range(0,len(prob)):
+      if prob[i] >= intervals[0] and prob[i] <= intervals[1]:
+          hdi_prob.append(prob[i])
+
+  hdi_prob = np.array(hdi_prob)
+  hdi_prob = np.sort(hdi_prob)
+  plt.figure()
+  plt.plot(hdi_prob)
+  plt.title(f'GRE = {gre} GPA = {gpa}')
+  plt.show()
+
+
+compute_probability(550,3.5)
+compute_probability(500,3.2)
+
+# in ambele cazuri studentul nu va fi admis, avand o probabilitatea prea mica de a fi admis
+# primul student are o probabilitate mai mare de a fi admis dar diferenta nu este foarte semnificativa,
+# acest lucru putand fi datorat inconsistentelor din setul de date
+
+
